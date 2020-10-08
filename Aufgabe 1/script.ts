a0fe94a05b679d56885ca1bf0234add4723bd5bf
amandaauer <48826856+amandaauer@users.noreply.github.com>
2020-10-08 12:45:51 +0200

    .

--- Aufgabe 1/script.ts
+++ Aufgabe 1/script.ts
@@ -1,7 +1,15 @@
 // Aufgabe 1//
-console.log("Handy1");
-
-window.onload = function () {
-    
+let nn = [];
+var x;
+Check = window.prompt('Geben Sie Ihren Namen ein', '');
+if (Check == '') {
+    alert('Keine Angabe');
 }
+else {
+    nn.push(Check);
+    console.log(nn[0]);
+}
+window.addEventListener("load", function () {
+    document.getElementById("Erster Schritt").innerHTML = nn[0];
+});
 //# sourceMappingURL=script.js.map
