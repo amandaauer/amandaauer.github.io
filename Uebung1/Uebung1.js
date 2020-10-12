0048f349ebce33e25f75fb956f2578008cca71d0
amandaauer <48826856+amandaauer@users.noreply.github.com>
2020-10-12 20:40:23 +0200

    .

---
+++ Uebung1/Uebung1.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    var person = prompt("Sag mir deinen Namen!", "Hier bitte");
+    document.getElementById("Hier ist dein Name: ").innerHTML = "Hier ist dein Name: " + person + "!";
+};
