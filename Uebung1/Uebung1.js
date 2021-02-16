bf685b0f5695160cb50cb3bfb8ea0816acb23eb2
Amanda <amanda.auer@hs-furtwangen.de>
2021-02-16 10:17:16 +0100

    .

--- Uebung1/Uebung1.js
+++
@@ -1,4 +0,0 @@
-window.onload = function () {
-    var person = prompt("Sag mir deinen Namen!", "Hier bitte");
-    document.getElementById("Hier ist dein Name: ").innerHTML = "Hier ist dein Name: " + person + "!";
-};
--- Uebung1/Uebung1.ts
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    let person = prompt("Sag mir deinen Namen!", "Hier bitte");
-    
-        document.getElementById("Hier ist dein Name: ").innerHTML = "Hier ist dein Name: " + person;
-    
-}
